25ff5c998f991c442c039fb6804be6af5749a856
swadeepmohanty <swadeepmohanty@gmail.com>
2023-11-14 14:17:57 +0530

    LinkedList first commit

---
+++ src/com/ds/linkedlist/LinkedListRunner.java
@@ -0,0 +1,11 @@
+package com.ds.linkedlist;
+
+public class LinkedListRunner {
+    public static void main(String[] args) {
+        LinkedListDemo linkedListDemo = new LinkedListDemo(3);
+        linkedListDemo.printLinkedList();
+        linkedListDemo.getHeadValue();
+        linkedListDemo.getTailValue();
+        linkedListDemo.getLength();
+    }
+}
